53b4c163100031277cd864be2003cc4bfd7db099
Adam Kimball <adamchasekimball@gmail.com>
2018-03-06 09:37:13 -0700

    Recipe List

--- recipe-maker/src/App.js
+++ recipe-maker/src/App.js
@@ -6,12 +6,47 @@ import Recipes from './Recipes';
 import './App.css';
 
 class App extends Component {
+  state = {
+    recipes: [
+      {
+        name: 'Beef Wellington',
+        ingredients: [
+          '- 400g flat cap mushrooms, roughly chopped',
+          '- sea salt and freshly ground black pepper',
+          '- olive oil, for cooking',
+          '- 750g piece of prime beef fillet',
+          '- 1-2tbsp English mustard',
+          '- 6-8 slices of Parma ham',
+          '- 500g ready-made puff pastry',
+          '- flour, to dust',
+          '- 2 egg yolks, beaten',
+        ]
+      },
+      {
+        name: 'Kimchi Jjigae',
+        ingredients: [
+          '- 1 pound kimchi, cut into bite size pieces',
+          '- ¼ cup kimchi brine',
+          '- ½ pound pork shoulder (or pork belly)',
+          '- ½ package of tofu (optional), sliced into ½ inch thick bite size pieces',
+          '- 1 teaspoon salt',
+          '- 2 teaspoons sugar',
+          '- 2 teaspoons gochugaru (Korean hot pepper flakes)',
+          '- 1 tablespoon gochujang (hot pepper paste)',
+          '- 1 teaspoon sesame oil',
+          '- 2 cups of anchovy stock (or chicken or beef broth)',
+        ]
+      },
+    ]
+  }
+
   render() {
+    const { recipes } = this.state;
     return (
       <div style={styles.app}>
         <div style={styles.leftColumn}>
           <MenuController />
-          <RecipeList />
+          <RecipeList recipes={recipes} />
         </div>
         <div style={styles.rightColumn}>
           <Recipes />
--- recipe-maker/src/RecipeList.js
+++ recipe-maker/src/RecipeList.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const RecipeList = () => (
-  <div>RecipeList</div>
-)
+const RecipeList = ({ recipes }) => {
+  return(
+    <div style={styles.recipeList}>
+      <p style={styles.titleFont}>RECIPE LIST:</p>
+      {recipes.map( recipe => {
+        return(
+          <div style={styles.recipeButton}>
+              <p style={styles.titleFont}>{recipe.name}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const styles = {
+  recipeButton: {
+    height: '26px',
+    width: '17vw',
+    padding: '10px',
+    border: '1px black solid',
+    margin: '5px',
+    backgroundColor: '#FFE37D',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleFont: {
+    fontSize: 30,
+    margin: 0,
+  },
+  recipeList: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+}
 
 export default RecipeList;
